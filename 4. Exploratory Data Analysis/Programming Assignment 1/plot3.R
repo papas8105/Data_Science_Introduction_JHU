f31517a33283203ea7bd3d7c30e5d74a66aa6f6f
George Papadopoulos <pgeorgios8@gmail.com>
2020-10-15 20:02:30 +0300

    corrected plot name
    mod4 pa correction

--- 4. Exploratory Data Analysis/Programming Assignment 1/plot3.R
+++ 4. Exploratory Data Analysis/Programming Assignment 1/plot3.R
@@ -5,5 +5,5 @@ lines(strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S"),as.double(data$Su
 lines(strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S"),as.double(data$Sub_metering_3),type = 'l',
       ylab = "Energy sub metering",col = 'blue')
 legend('topright',lty = 1,col = c('black','red','blue'),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.copy(png,"png3.png")
+dev.copy(png,"plot3.png")
 dev.off()
