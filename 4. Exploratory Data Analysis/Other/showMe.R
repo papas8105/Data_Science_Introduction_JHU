8ef58a64c69415c7c0de57496151963cc4b858e8
George Papadopoulos <pgeorgios8@gmail.com>
2021-07-01 01:48:45 +0300

    course 4
    add material

--- 4. Exploratory Data Analysis/Other/showMe.R
+++ 4. Exploratory Data Analysis/Other/showMe.R
@@ -1,7 +1,7 @@
 # function to visualize colors' range create by coloRamp/ColoRampPalette/RColorBrewer
-function(cv){
+function(cv) {
   myarg <- deparse(substitute(cv))
-  z<- outer( 1:20,1:20, "+")
-  obj<- list( x=1:20,y=1:20,z=z )
-  image(obj, col=cv, main=myarg  )
+  z  <- outer(1:20,1:20,"+")
+  obj <- list(x = 1:20,y = 1:20,z = z)
+  image(obj,col = cv,main = myarg)
 }
